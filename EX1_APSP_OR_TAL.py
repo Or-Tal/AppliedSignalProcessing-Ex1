56d3641a2e3b8512294f2e6e38da803cb097578e
ortal1602 <47923357+ortal1602@users.noreply.github.com>
2020-05-04 18:12:53 +0300

    done 5

--- EX1_APSP_OR_TAL.py
+++ EX1_APSP_OR_TAL.py
@@ -1,8 +1,11 @@
 from EX1_APSP_UTILS import *
 import matplotlib.pyplot as plt
 
-AIRPLANE = "./"
-
+AIRPLANE = "airplane.wav"
+CAFE = "cafe.wav"
+CITY = "city.wav"
+VACCUM = "vacuumcleaner.wav"
+DIR = "./external"
 
 
 def Q1_sec4():
@@ -94,13 +97,13 @@ def Q2():
 
 
 def Q3():
-    alpha, sigma, t = 0.9, 0.5, 2
+    alpha, sigma, t = 0.9, 0.5, 10
     L = [1, 2, 4]
     Mu = [0.01, 0.001, 0.0001]
     signal = gen_wss_signal(alpha, sigma, t, 48, 's', 'khz')
     print("section 2:")
     fig, axs = plt.subplots(3, 3, figsize=(15, 15))
-    step = 1000
+    step = 1500
     for i, l in enumerate(L):
         R, P = gen_R_mat_and_P(alpha, sigma, l)
         print("L = {}, R's largest eigenvalue is ~ {}".format(l, np.round(np.max(np.linalg.eigvals(R)), 4)))
@@ -125,18 +128,22 @@ def Q3():
             axs[i, j].set(xlabel="n (sample num.)",
                           ylabel="relative error in db")
             axs[i, j].legend(["coefficient err", "cumulative err mean line", "cumulative NRdb"])
+
+            play_audio(signal[l:100000 + l], 48000)
+            play_audio(signal[l:100000 + l] - predictions[:100000].flatten(), 48000)
+
     plt.show()
 
 
 def Q4():
     # section 1
-    lam, L, alpha, sigma, t, reps, step = 0.99, 2, 0.9, 0.5, 10, 3, 1000
+    lam, L, alpha, sigma, t, reps, step = 0.99, 2, 0.9, 0.5, 10, 3, 1200
 
     signal = gen_wss_signal(alpha, sigma, t, 48, 's', 'khz')  # initiate the signal
     Delta = np.linspace(1e-5, 5, reps * 2)
     Delta[-1] = 100
     w_star = optimal_est(alpha, sigma, L)
-
+    calc_p2 = calc_P2(signal[:5])
     fig, axs = plt.subplots(2, reps, figsize=(16, 8))
     print("Q4 section 1")
     i, j = 0, 0
@@ -173,7 +180,7 @@ def Q4():
             i += 1
         print("calc subsection: {}, {}".format(i, j))
         # get the prediction err and coefficints as a function of iterations
-        predictions, coefficients = RLS(signal, L, d, lam)
+        predictions, coefficients, P2 = RLS(signal, L, d, lam, True)
         err_filter_coef = calc_err_filter_coef(coefficients, w_star)
         rel_predict_err = NRdb(signal, predictions.flatten())
         mean_line = fit_mean_line(np.asarray(err_filter_coef))
@@ -188,6 +195,9 @@ def Q4():
         axs[i, j].set_ylabel("relative error in db")
         axs[i, j].legend(["coefficient err", "cumulative err mean line"])
 
+        # compare predicted and actual P2
+        print("delta = {}, predicted P2:\n{}".format(d, P2))
+        print("calculated P2:\n{}".format(calc_p2))
         j += 1
 
     plt.show()
@@ -195,13 +205,134 @@ def Q4():
 
 def Q5(play=True):
     trivial_est = np.ones(1)
+    sounds_compare = []
+    for sound in [CITY, CAFE, AIRPLANE, VACCUM]:
+        trivials, RLS_out, LMS_out = [], [], []
+        N = 400000
+        signal = read_audio_file("{}/{}".format(DIR, sound))[:N]
+        trivial_estimated_signal = signal - predict(signal, trivial_est)
+        trivials.append(trivial_estimated_signal)
+        step = 1500
+
+        fig, axs = plt.subplots(5, 3, figsize=(30, 40))
+        for k, L in enumerate([2, 4, 8, 20, 30]):
+            line_RLS = []
+            for i, lam in enumerate([0.3, 0.6, 0.99]):
+                predictions, coefficients = RLS(signal, L, lam=lam)
+                audio = signal.flatten() - predictions.flatten()
+                line_RLS.append(audio)
+                if play:
+                    print("playing benchmark")
+                    play_audio(trivial_estimated_signal[:N], 48000)
+                    print("playing RLS L={} lambda={}".format(L, lam))
+                    play_audio(audio[:N], 48000)
+                    print("done")
+                mse_ = mse(signal[500:], predictions[500:])
+                nrdb = NRdb(signal[500:], predictions[500:])
+                axs[k, i].plot(np.arange(500, audio.shape[0], step), audio[500::step], c='red')
+                axs[k, i].set_title("{}, RLS, L = {}, lam = {}\n"
+                                    "NRdb = {}, MSE = {}".format(sound, L, lam, np.round(nrdb, 3), np.round(mse_, 6)), fontsize=22)
+                axs[k, i].grid()
+            RLS_out.append(line_RLS)
+        plt.show()
+
+        fig, axs = plt.subplots(4, 5, figsize=(30, 20))
+        for k, L in enumerate(range(1, 5)):
+            line_LMS = []
+            for i, mu in enumerate([0.1, 0.05, 0.01, 0.005, 0.001]):
+                coefficients, predictions = LMS(signal, mu, L)
+                audio = signal.flatten()[L:] - predictions.flatten()
+                line_LMS.append(audio)
+                if play:
+                    print("playing benchmark")
+                    play_audio(trivial_estimated_signal[:N], 48000)
+                    print("playing LMS L={} mu={}".format(L, mu))
+                    play_audio(audio[:N], 48000)
+                    print("done")
+                mse_ = mse(signal[L + 500:], predictions[500:])
+                nrdb = NRdb(signal[L + 500:], predictions[500:])
+                axs[k, i].plot(np.arange(500, audio.shape[0], step), audio[500::step], c='red')
+                axs[k, i].set_title("{}, LMS, L = {}, mu = {}\n"
+                                    "NRdb = {}, MSE = {}".format(sound, L, mu, np.round(nrdb, 3), np.round(mse_, 6)), fontsize=22)
+                axs[k, i].grid()
+
+            LMS_out.append(line_LMS)
+        plt.show()
+
+        sounds_compare.append([trivials, LMS_out, RLS_out])
+    return sounds_compare
 
 
+def Q5_results():
+    # after preforming some experiments chose the best values for each plot
+    best_LMS_ARGS = [(3, 0.1, 35000), (3, 0.1, 40000), (3, 0.1, 40000), (4, 0.1, 4000)]
+    best_RLS_ARGS = [(8, 0.99, 100), (20, 0.99, 100), (8, 0.99, 100), (20, 0.99, 100)]
+    M = 10000
+
+    for k, sound in enumerate([CITY, CAFE, AIRPLANE, VACCUM]):
+        fig, axs = plt.subplots(1, 2, figsize=(16, 8))
+        signal = read_audio_file("{}/{}".format(DIR, sound))
+        IP_signal = instantaneous_power(signal, M)
+
+        # LMS
+        L1, mu, conv_idx1 = best_LMS_ARGS[k]
+        coefficients, predictions = LMS(signal, mu, L1)
+        audio1 = signal.flatten()[L1:] - predictions.flatten()
+        nrdb1 = NRdb(signal[conv_idx1 + L1:], predictions[conv_idx1:])
+        IP_LMS = instantaneous_power(audio1, M)
+        axs[0].plot(np.arange(IP_signal.shape[0]), IP_signal, c="blue")
+        axs[0].plot(np.arange(IP_LMS.shape[0]), IP_LMS, c="red")
+        axs[0].set_title("{}, RLS,L = {}, $\mu$ = {}, noise reduction = {}[dB]".format(sound, L1, mu, np.round(nrdb1, 3)))
+        axs[0].set_xlabel("sample number")
+        axs[0].set_ylabel("instantaneous power [db]")
+        axs[0].grid()
+        axs[0].legend(["original noise power", "prediction error power"])
+
+        # RLS
+        L2, lam, conv_idx2 = best_RLS_ARGS[k]
+        predictions, coefficients= RLS(signal, L2, 1e-5, lam)
+        audio2 = signal.flatten() - predictions.flatten()
+        nrdb2 = NRdb(signal[conv_idx2:], predictions[conv_idx2:])
+        IP_RLS = instantaneous_power(audio2, M)
+        axs[1].plot(np.arange(IP_signal.shape[0]), IP_signal, c="blue")
+        axs[1].plot(np.arange(IP_RLS.shape[0]), IP_RLS, c="red")
+        axs[1].set_title("{}, RLS,L = {}, $\mu$ = {}, noise reduction = {}[dB]".format(sound, L1, mu, np.round(nrdb2, 3)))
+        axs[1].set_xlabel("sample number")
+        axs[1].set_ylabel("instantaneous power [db]")
+        axs[1].grid()
+        axs[1].legend(["original noise power", "prediction error power"])
+
+        plt.show()
+
+
+def Q6(zvec):
+    z = _preprocess_Q6(zvec)
+    err = 10 ** 10
+    znext = None
+    for L in [3, 6, 9, 20]:
+        for lam in [0.3, 0.6, 0.99]:
+            predictions, coefficients = RLS(z, L, lam=lam)
+            N = predictions.shape[0]
+            _mse = mse(z[N // 10:], predictions[N // 10:])  # assuming that after 10% RLS will converge
+            if _mse < err:
+                u = to_col_vec(predictions[-L:])
+                w = to_col_vec(coefficients[-1])
+                znext = np.matmul(u.T, w).flatten()[0]
+                err = _mse
+    return znext
+
+
+def _preprocess_Q6(zvec):
+    z = np.asarray(zvec).flatten()
+    N = min(48000, z.shape[0])
+    return z[z.shape[0] - N: z.shape[0]]
 
 
 if __name__ == "__main__":
     # Q1_sec4()
     # Q1_sec5(False)
     # Q2()
-    Q3()
-    Q4()
+    # Q3()
+    # Q4()
+    # sounds = Q5(False)
+    Q5_results()
--- EX1_APSP_UTILS.py
+++ EX1_APSP_UTILS.py
@@ -4,47 +4,131 @@ import numpy as np
 import scipy.signal as ss
 import scipy.linalg as sl
 import sounddevice as sd
+from scipy.io.wavfile import read, write
 
 
-def predict(signal: np.ndarray, w: np.ndarray):
-    signal, w = signal.flatten(), w.flatten()
-    w = np.pad(w, (1, 0))
-    prediction = ss.lfilter(w, [1], signal)
-    return prediction.flatten()
-
-
-def play_audio(audio: np.ndarray, frec):
+# --------------- Algorithms ---------------
+def steepest_descent(w0, mu, N, alpha, sigma, L, wstar=None):
     """
-    :param audio: array of audio input
-    :param frec: sampling frequency in Hz
-    :return:
+    stages:
+    1. calculate auto-correlation matrix R and cross-correlation vector P
+    2. for i=1,...,N
+        update coefficient: w_(n+1) = w_n + mu(P - R @ w_n)  // where mu = 0.5 * mu_tilda
+    :param w0:          initial coefficient vector
+    :param mu:    step factor
+    :param N:           num of iterations
+    :param alpha, sigma: parameters of the signal
+    :param L:           order of the estimator
+    :return:            estimated w + optional: squared norm err of each iteration
     """
-    sd.play(audio, samplerate=frec, blocking=True)
+    w = to_col_vec(w0)  # initial coefficient vector
+    wstar = to_col_vec(wstar)
+    # calculate R, P
+    R, P = gen_R_mat_and_P(alpha, sigma, L)
+    P = to_col_vec(P)
+
+    Cn = []
+    for i in np.arange(N):
+        # update coefficient
+        w = w + mu * (P - np.matmul(R, w)) / 2
+
+        # update output err array
+        if wstar is not None:
+            e = np.matmul((w - wstar).T, (w - wstar))[0][0]
+            Cn.append(e)
+
+    if wstar is not None:
+        return w, Cn
+    return w
 
 
-def calc_num_sample_for_t_in_freq(t, freq, tunit='s', funit='hz'):
+def LMS(signal: np.ndarray, mu, L):
     """
-    calculates how many samples should be in t (seconds) at frequency freq (unit)
-    :param t: num of time units
-    :param freq: num of frequency units
-    :param tunit: time unit
-    :param funit: freq unit
-    :return: num of samples
+    Least mean squares algorithm
+    stages:
+    1.  choose initial guess for w
+    2.  for each n = 0,1,2,... do:
+        a.  Dn_est = w^T @ Un
+        b.  Calculate the estimation error: en = Dn - Dn_est
+        c.  Update the weights according to: w_(n+1) = w_n + mu * Un * en
+
+    :param signal: np array = vector -> signal to estimate
+    :param mu: step size
+    :param L: num of coefficients in filter
+    :return: predicted signal starting from L'th coordinate, list of all w coefficients during iterations
     """
-    assert freq != 0
-    funits = {'hz': 0, 'khz': 3, 'mhz': 6, 'ghz': 9}
-    tunits = {'s': 0, 'ms': -3, 'micros': -6, 'ns': -9}
-    assert type(funit) == str
-    assert type(tunit) == str
-    funit = funit.lower()
-    tunit = tunit.lower()
-    assert funit in funits.keys(), "unsupported unit, available units: hz, khz, mhz, ghz"
-    assert tunit in tunits.keys(), "unsupported unit, available units: s, ms, micros, ns"
-    freq = freq * (10 ** (funits[funit]))
-    t = t * (10 ** (tunits[tunit]))
-    return int(np.round(t * freq))
+    assert len(signal.shape) <= 2
+    assert mu > 0
+    if len(signal.shape) == 1 or signal.shape[1] == 1:
+        signal = to_col_vec(signal)
+
+    # initiate the coefficient vector, create output arrays
+    w = np.zeros((L, 1))
+    coefficients, predictions = [], []
+
+    for n in np.arange(L, signal.shape[0]):
+        # construct the estimator
+        Un = np.flip(signal[n - L:n])
+        Dn_est = np.matmul(w.T, Un).flatten()[0]
+
+        # compute estimation err
+        en = signal[n][0] - Dn_est
+
+        # update weights vector
+        w = w + mu * Un * en
+
+        # update output arrays
+        coefficients.append(w.flatten())
+        predictions.append(Dn_est)
+
+    coef_shape = np.shape(coefficients)
+    pred_shape = np.shape(predictions)
+    return np.asarray(coefficients).reshape(coef_shape), np.asarray(predictions).reshape(pred_shape)
 
 
+def RLS(signal: np.ndarray, L, delta=1e-2, lam=0.99, p2_flag=False):
+    """
+    implementation of recursive least squares
+    :param signal: ground truth signal
+    :param L: num of coefficients in filter
+    :param delta, lam: the model's parameters
+    :return: list of the estimations, and a list of the coefficients in each iteration
+    """
+    # init output arrays
+    estimations, coefficients = [], []
+
+    # initialize P, w
+    signal = np.pad(signal, (L, 0))
+    P, w = np.eye(L) / delta, np.zeros((L, 1))
+    P2 = 0
+    for n in np.arange(L, signal.shape[0]):
+        # compute the estimated value Dn
+        Un = np.flip(signal[n - L:n]).reshape((L, 1))
+        Dn_est = np.matmul(w.T, Un).flatten()
+        estimations.append(Dn_est[0])  # add to output array
+
+        # compute the estimation err
+        en = (signal[n] - Dn_est)[0]
+
+        # compute kn
+        kn = (np.matmul(P, Un) / lam) / (1 + np.matmul(Un.T, np.matmul(P, Un)) / lam)
+
+        # update weight vector
+        w = w + kn * en
+        coefficients.append(w.flatten())  # add to output array
+
+        # update P matrix
+        P = P / lam - np.matmul(kn, np.matmul(Un.T, P)) / lam
+
+        # save P2 for the output for comparison of delta
+        if n == 2:
+            P2 = P
+    if p2_flag:
+        return np.asarray(estimations).flatten(), coefficients, P2
+    return np.asarray(estimations).flatten(), coefficients
+
+
+# --------------- signal operations ---------------
 def gen_wss_signal(alpha, sigma, t, freq, tunit='s', funit='hz'):
     """
     generate WSS signal corresponding to t (tunit) for audio sampling freq (funit) frequency
@@ -82,40 +166,6 @@ def gen_R_mat_and_P(alpha, sigma, L):
     return R, P
 
 
-def steepest_descent(w0, mu, N, alpha, sigma, L, wstar=None):
-    """
-    stages:
-    1. calculate auto-correlation matrix R and cross-correlation vector P
-    2. for i=1,...,N
-        update coefficient: w_(n+1) = w_n + mu(P - R @ w_n)  // where mu = 0.5 * mu_tilda
-    :param w0:          initial coefficient vector
-    :param mu:    step factor
-    :param N:           num of iterations
-    :param alpha, sigma: parameters of the signal
-    :param L:           order of the estimator
-    :return:            estimated w + optional: squared norm err of each iteration
-    """
-    w = to_col_vec(w0)  # initial coefficient vector
-    wstar = to_col_vec(wstar)
-    # calculate R, P
-    R, P = gen_R_mat_and_P(alpha, sigma, L)
-    P = to_col_vec(P)
-
-    Cn = []
-    for i in np.arange(N):
-        # update coefficient
-        w = w + mu * (P - np.matmul(R, w)) / 2
-
-        # update output err array
-        if wstar is not None:
-            e = np.matmul((w - wstar).T, (w - wstar))[0][0]
-            Cn.append(e)
-
-    if wstar is not None:
-        return w, Cn
-    return w
-
-
 def optimal_est(alpha, sigma, L):
     """calculates the optimal estimator for the given params"""
     # get R matrix and P vector
@@ -124,95 +174,8 @@ def optimal_est(alpha, sigma, L):
     return np.matmul(np.linalg.inv(R), P).flatten()
 
 
-def to_col_vec(vec: np.ndarray):
-    """transforms vector to col vector"""
-    vec = vec.flatten()
-    return vec.reshape((vec.shape[0], 1))
-
-
-def LMS(signal: np.ndarray, mu, L):
-    """
-    Least mean squares algorithm
-    stages:
-    1.  choose initial guess for w
-    2.  for each n = 0,1,2,... do:
-        a.  Dn_est = w^T @ Un
-        b.  Calculate the estimation error: en = Dn - Dn_est
-        c.  Update the weights according to: w_(n+1) = w_n + mu * Un * en
-
-    :param signal: np array = vector -> signal to estimate
-    :param mu: step size
-    :param L: num of coefficients in filter
-    :return: predicted signal starting from L'th coordinate, list of all w coefficients during iterations
-    """
-    assert len(signal.shape) <= 2
-    assert mu > 0
-    if len(signal.shape) == 1 or signal.shape[1] == 1:
-        signal = to_col_vec(signal)
-
-    # initiate the coefficient vector, create output arrays
-    w = np.zeros((L, 1))
-    coefficients, predictions = [], []
-
-    for n in np.arange(L, signal.shape[0]):
-        # construct the estimator
-        Un = signal[n - L:n]
-        Dn_est = np.matmul(w.T, Un)[0]
-
-        # compute estimation err
-        en = signal[n] - Dn_est
-
-        # update weights vector
-        w = w + mu * Un * en
-
-        # update output arrays
-        coefficients.append(w.flatten())
-        predictions.append(Dn_est)
-
-    coef_shape = np.shape(coefficients)
-    pred_shape = np.shape(predictions)
-    return np.asarray(coefficients).reshape(coef_shape), np.asarray(predictions).reshape(pred_shape)
-
-
-def RLS(signal: np.ndarray, L, delta, lam):
-    """
-    implementation of recursive least squares
-    :param signal: ground truth signal
-    :param L: num of coefficients in filter
-    :param delta, lam: the model's parameters
-    :return: list of the estimations, and a list of the coefficients in each iteration
-    """
-    # init output arrays
-    estimations, coefficients = [], []
-
-    # initialize P, w
-    signal = np.pad(signal, (L, 0))
-    P, w = np.eye(L) / delta, np.zeros((L, 1))
-    for n in np.arange(L, signal.shape[0]):
-        # compute the estimated value Dn
-        Un = signal[n - L:n].reshape((L, 1))
-        Dn_est = np.matmul(w.T, Un).flatten()
-        estimations.append(Dn_est[0])  # add to output array
-
-        # compute the estimation err
-        en = (signal[n] - Dn_est)[0]
-
-        # compute kn
-        kn = (np.matmul(P, Un) / lam) / (1 + np.matmul(Un.T, np.matmul(P, Un)) / lam)
-
-        # update weight vector
-        w = w + kn * en
-        coefficients.append(w.flatten())  # add to output array
-
-        # update P matrix
-        P = P / lam - np.matmul(kn, np.matmul(Un.T, P)) / lam
-
-    return np.asarray(estimations).flatten(), coefficients
-
-
 def optimal_est_RLS(signal: np.ndarray, L, lam):
     """
-
     :param signal:
     :param L:
     :param lam:
@@ -225,7 +188,7 @@ def optimal_est_RLS(signal: np.ndarray, L, lam):
     # initiate phy and gn
     phy, gn = lam * np.matmul(signal[:L], signal[:L].T), lam * signal[:L] * signal[L, 0]
     for i in np.arange(L, signal.shape[0]):
-        ui = signal[i - L:i, :]
+        ui = signal[i:i - L:-1, :]
         di = signal[i, 0]
 
         # update phy, gn
@@ -238,34 +201,42 @@ def optimal_est_RLS(signal: np.ndarray, L, lam):
     return coefficients
 
 
-def RLS_relative_coef_err(optimal, predicted):
-    """
-    :param optimal: optimal coefficient per iteration
-    :param predicted: predicted coeficient per iteration
-    :return: relative coefficient error per iteration
-    """
-    opt_shape = np.shape(optimal)
-    optimal = np.asarray(optimal).reshape(opt_shape)
-    predicted = np.asarray(predicted).reshape(opt_shape)  # should have same shape
-
-    Cn_sq = np.linalg.norm(predicted - optimal, axis=1) ** 2
-    return 10 * np.log10(Cn_sq / (np.linalg.norm(optimal, axis=1) ** 2))
+def predict(signal: np.ndarray, w: np.ndarray):
+    signal, w = signal.flatten(), w.flatten()
+    w = np.pad(w, (1, 0))
+    prediction = ss.lfilter(w, [1], signal)
+    return prediction.flatten()
 
 
-def RLS_prediction_err(signal: np.ndarray, prediction: np.ndarray, lam):
-    """
+# --------------- functional calculations ---------------
+def mse(z: np.ndarray, z_hat: np.ndarray):
+    assert z.shape[0] == z_hat.shape[0]
+    z, z_hat = to_col_vec(z), to_col_vec(z_hat)
+    err = np.matmul((z-z_hat).T, (z-z_hat)).flatten()[0]/(z.shape[0])
+    return  err
 
-    :param signal: ground truth signal
-    :param prediction: predicted signal
-    :param lam: forgetfulness factor
-    :return:
+
+def calc_num_sample_for_t_in_freq(t, freq, tunit='s', funit='hz'):
     """
-    signal, prediction = signal.flatten(), prediction.flatten()
-    out_arr = np.ones(signal.shape[0])
-    out_arr[0] = (signal[0] - prediction[0]) ** 2
-    for i in np.arange(1, signal.shape[0]):
-        out_arr[i] = out_arr[i - 1] / lam + (signal[i] - prediction[i]) ** 2
-    return out_arr
+    calculates how many samples should be in t (seconds) at frequency freq (unit)
+    :param t: num of time units
+    :param freq: num of frequency units
+    :param tunit: time unit
+    :param funit: freq unit
+    :return: num of samples
+    """
+    assert freq != 0
+    funits = {'hz': 0, 'khz': 3, 'mhz': 6, 'ghz': 9}
+    tunits = {'s': 0, 'ms': -3, 'micros': -6, 'ns': -9}
+    assert type(funit) == str
+    assert type(tunit) == str
+    funit = funit.lower()
+    tunit = tunit.lower()
+    assert funit in funits.keys(), "unsupported unit, available units: hz, khz, mhz, ghz"
+    assert tunit in tunits.keys(), "unsupported unit, available units: s, ms, micros, ns"
+    freq = freq * (10 ** (funits[funit]))
+    t = t * (10 ** (tunits[tunit]))
+    return int(np.round(t * freq))
 
 
 def cum_NRdb(Z: np.ndarray, Z_hat: np.ndarray):
@@ -303,7 +274,76 @@ def calc_err_filter_coef(coef_lst, optimal_est: np.ndarray):
     return err_arr
 
 
+def instantaneous_power(signal: np.ndarray, M: int):
+    """
+    calculates the instantaneous power of the signal
+    according to the formula: 1/M * sum^(M-1)_(l=0){signal[n-l] ** 2}
+    :param signal: signal to estimate
+    :param M: window width
+    :return: result according to formula, in db scale
+    """
+    signal = signal.flatten() ** 2
+    filter = np.ones(M)
+    err = ss.lfilter(filter, [1], signal) / M
+    return 10 * np.log10(err)
+
+
+def RLS_relative_coef_err(optimal, predicted):
+    """
+    :param optimal: optimal coefficient per iteration
+    :param predicted: predicted coeficient per iteration
+    :return: relative coefficient error per iteration
+    """
+    opt_shape = np.shape(optimal)
+    optimal = np.asarray(optimal).reshape(opt_shape)
+    predicted = np.asarray(predicted).reshape(opt_shape)  # should have same shape
+
+    Cn_sq = np.linalg.norm(predicted - optimal, axis=1) ** 2
+    return 10 * np.log10(Cn_sq / (np.linalg.norm(optimal, axis=1) ** 2))
+
+
+def RLS_prediction_err(signal: np.ndarray, prediction: np.ndarray, lam):
+    """
+
+    :param signal: ground truth signal
+    :param prediction: predicted signal
+    :param lam: forgetfulness factor
+    :return:
+    """
+    signal, prediction = signal.flatten(), prediction.flatten()
+    out_arr = np.ones(signal.shape[0])
+    out_arr[0] = (signal[0] - prediction[0]) ** 2
+    for i in np.arange(1, signal.shape[0]):
+        out_arr[i] = out_arr[i - 1] / lam + (signal[i] - prediction[i]) ** 2
+    return out_arr
+
+
+def calc_P2(u: np.ndarray):
+    """generates P2 matrix for comparison"""
+    u = to_col_vec(u)
+    lam = 0.99
+    phi = lam * np.matmul(u[:2], u[:2].T) + np.matmul(u[1:3], u[1:3].T)
+    return np.linalg.inv(phi.reshape((2,2)))
+
+
+# --------------- General utils ---------------
+def to_col_vec(vec: np.ndarray):
+    """transforms vector to col vector"""
+    vec = vec.flatten()
+    return vec.reshape((vec.shape[0], 1))
+
+
+def play_audio(audio: np.ndarray, frec):
+    """
+    :param audio: array of audio input
+    :param frec: sampling frequency in Hz
+    :return:
+    """
+    sd.play(audio, samplerate=frec, blocking=True)
+
+
 def fit_mean_line(input: np.ndarray):
+    """fits an average line for cumulative input"""
     input = input.flatten()
     cum_signal = np.cumsum(input)
     output = cum_signal / np.arange(1, input.shape[0] + 1)
@@ -311,3 +351,17 @@ def fit_mean_line(input: np.ndarray):
 
 
 def read_audio_file(path):
+    """reads audio file and normalize it's values"""
+    audio = read(path)[1]
+    return normalize(audio)
+
+
+def normalize(arr: np.ndarray):
+    m1 = np.max(arr)
+    m2 = np.min(arr)
+    val = max(m1, abs(m2))
+    return arr / val
+
+
+def save_audio_file(path, data):
+    return write(path, 48000, data)
